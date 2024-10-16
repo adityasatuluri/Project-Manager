010f0fa36403f7344ba495c07e7f950daa74350f
Aditya Satuluri <s.aditya.in@gmail.com>
2024-10-16 19:53:14 +0530

    keys

--- database.py
+++ database.py
@@ -3,5 +3,5 @@ import streamlit as st
 #from config import MONGO_URI, DATABASE_NAME
 
 def get_database():
-    client = MongoClient(st.secrets[MONGO_URI])
-    return client[st.secrets[DATABASE_NAME]]
+    client = MongoClient(st.secrets['MONGO_URI'])
+    return client[st.secrets['DATABASE_NAME']]
