0b737747e4c2a668d517cd8de5842bb7ac48b9ac
Aditya Satuluri <s.aditya.in@gmail.com>
2024-10-16 20:00:28 +0530

    test

--- database.py
+++ database.py
@@ -5,3 +5,6 @@ import streamlit as st
 def get_database():
     client = MongoClient(st.secrets['MONGO_URI'])
     return client[st.secrets['DATABASE_NAME']]
+
+MONGO_URI = "mongodb+srv://sadityain:dTnronjU9pdDsp92@cluster0.6f5iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE_NAME = "project_management"
